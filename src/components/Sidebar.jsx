54dc0b839defb76dcfc4e24a77ddd5927773bde1
Filipe Almeida <filipesilva504@gmail.com>
2023-02-06 16:03:39 -0300

    feat created avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -9,10 +10,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar}
-                    src="https://avatars.githubusercontent.com/u/31519661?v=4" 
-                />
+                <Avatar src="https://avatars.githubusercontent.com/u/31519661?v=4" />
 
                 <strong>Filipe Almeida</strong>
                 <span>Web Developer</span>
